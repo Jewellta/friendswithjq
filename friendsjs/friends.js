04f1c8aa5dc5620761536eec299834a6d9ed1fbd
Tyler Jewell <jewelltyler15@gmail.com>
2021-03-26 09:49:09 -0400

    first

---
+++ friendsjs/friends.js
@@ -0,0 +1,34 @@
+let friends =[];
+
+$().ready(
+    ()=>{
+
+        $("#add").click(()=>{
+            let fname= $("#iname").val();
+            let femail= $("#iemail").val();
+            let fphone= $("#iphone").val();
+            let friend={
+            name: fname, email: femail, phone: fphone
+        };
+        friends.push(friend);
+        display();
+        });
+    }
+);
+
+const display = () => {
+    let tbody= $("tbody");
+    tbody.empty();
+    for(let friend of friends){
+        let tr =$("<tr></tr>");
+        let tdName= $(`<td>${friend.name}</td>`);
+        tr.append(tdName);
+
+        let tdEmail= $(`<td>${friend.email}</td>`);
+        tr.append(tdEmail);
+
+        let tdPhone= $(`<td>${friend.phone}</td>`);
+        tr.append(tdPhone);
+        tbody.append(tr);
+    }
+}
